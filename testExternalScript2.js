320c86d13e043ee9d5f39dbc2d6a21f66c5b1f66
M1nc3r4f573r <xenohoe2@gmail.com>
2024-09-24 21:49:51 +0800

    Create testExternalScript2.js

---
+++ testExternalScript2.js
@@ -0,0 +1,3 @@
+function getImageEmbed() {
+    return '<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Anterior_view_of_human_female.jpg/440px-Anterior_view_of_human_female.jpg" alt="Anterior view of human female">';
+}
